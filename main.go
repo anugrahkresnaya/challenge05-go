64152e99775fa0e2a4b0f817435b83d8f8f66e02
anugrahkresnaya <anugrahkresnaya.ak@gmail.com>
2023-03-17 16:46:20 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+  "sync"
+)
+
+func randomCondition() {
+  var wg sync.WaitGroup
+
+  cobaInterface := []string{"coba1", "coba2", "coba3"}
+  bisaInterface := []string{"bisa1", "bisa2", "bisa3"}
+  for i := 0; i <= 3; i++ {
+    wg.Add(2)
+    go func(i int) {
+      fmt.Println(cobaInterface, i + 1)
+      wg.Done()
+    }(i)
+    go func(i int) {
+      fmt.Println(bisaInterface, i + 1)
+      wg.Done()
+    }(i)
+  }
+  wg.Wait()
+}
+
+func main() {
+  fmt.Println("random condition:")
+  randomCondition()
+}
