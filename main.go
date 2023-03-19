12727f9039af2b4ba14a3be5469f3f1ed0a05aed
anugrahkresnaya <anugrahkresnaya.ak@gmail.com>
2023-03-19 11:42:45 +0700

    first commit

--- main.go
+++ main.go
@@ -24,6 +24,25 @@ func randomCondition() {
   wg.Wait()
 }
 
+func structuredCondition() {
+  var wg sync.WaitGroup
+  var mu sync.Mutex
+
+  cobaInterface := []string{"coba1", "coba2", "coba3"}
+  bisaInterface := []string{"bisa1", "bisa2", "bisa3"}
+
+  for i := 0; i <= 3; i++ {
+    wg.Add(2)
+    go func(i int) {
+      fmt.Println(cobaInterface, i + 1)
+    }
+
+    func(i int) {
+      
+    }
+  }
+}
+
 func main() {
   fmt.Println("random condition:")
   randomCondition()
